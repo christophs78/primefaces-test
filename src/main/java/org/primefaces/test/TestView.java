d6c8dbbf996f3f2e6c8c9f6543ebebede4b33539
Christoph Straßer <christoph.strasser@ooe.gv.at>
2024-03-08 13:43:11 +0100

    Timeline: reproducer for missing entries

--- src/main/java/org/primefaces/test/TestView.java
+++ src/main/java/org/primefaces/test/TestView.java
@@ -1,14 +1,19 @@
 package org.primefaces.test;
 
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.time.format.DateTimeFormatter;
+
 import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+
+import org.primefaces.event.timeline.TimelineSelectEvent;
+import org.primefaces.model.timeline.TimelineEvent;
+import org.primefaces.model.timeline.TimelineModel;
 
 import lombok.Data;
 
@@ -16,22 +21,95 @@ import lombok.Data;
 @Named
 @ViewScoped
 public class TestView implements Serializable {
-    
-    private String string;
-    private Integer integer;
-    private BigDecimal decimal;
-    private LocalDateTime localDateTime;
-    private List<TestObject> list;
-    
-    @PostConstruct  
-    public void init() {
-        string = "Welcome to PrimeFaces!!!";
-        list = new ArrayList<>(Arrays.asList(
-                new TestObject("Thriller", "Michael Jackson", 1982),
-                new TestObject("Back in Black", "AC/DC", 1980),
-                new TestObject("The Bodyguard", "Whitney Houston", 1992),
-                new TestObject("The Dark Side of the Moon", "Pink Floyd", 1973)
-        ));
-    }
 
+	private TimelineModel<String, ?> model;
+
+	private LocalDateTime start;
+	private LocalDateTime end;
+
+	private boolean additionalEntries = false;
+	private boolean extendedTimePeriod = false;
+
+	@PostConstruct
+	public void init() {
+		// set initial start / end dates for the axis of the timeline
+		start = LocalDate.now().minusDays(5).atStartOfDay();
+		end = LocalDate.now().plusDays(5).atStartOfDay();
+
+		createTimelineModel();
+	}
+
+	public void createTimelineModel() {
+		// create timeline model
+		model = new TimelineModel<>();
+
+		for (int i = 1; i <= 10; i++) {
+			int interleave = ((i * 2) - 11) * 2;
+
+			LocalDateTime startDate = LocalDate.now().plusDays(6).plusDays(interleave).atStartOfDay();
+			LocalDateTime endDate = startDate.plusDays(14);
+			if (extendedTimePeriod) {
+				endDate = endDate.plusMonths(1);
+				// endDate = LocalDate.of(2999, 12, 31).atStartOfDay();
+			}
+			TimelineEvent event = TimelineEvent.builder().data("Max " + i + timerange(startDate, endDate)).startDate(startDate)
+					.endDate(endDate).build();
+			model.add(event);
+
+			if (additionalEntries) {
+				startDate = LocalDate.now().minusDays(10).plusDays(interleave).atStartOfDay();
+				endDate = startDate.plusDays(12);
+				event = TimelineEvent.builder().data("Lewis " + i + timerange(startDate, endDate)).startDate(startDate).endDate(endDate)
+						.build();
+				model.add(event);
+			}
+		}
+	}
+
+	private String timerange(LocalDateTime start, LocalDateTime end) {
+		return " " + start.toLocalDate().format(DateTimeFormatter.ISO_LOCAL_DATE) + " - " + end.toLocalDate().format(DateTimeFormatter.ISO_LOCAL_DATE);
+	}
+
+	public void onSelect(TimelineSelectEvent<String> e) {
+		TimelineEvent<String> timelineEvent = e.getTimelineEvent();
+
+		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Selected event:", timelineEvent.getData());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	public TimelineModel<String, ?> getModel() {
+		return model;
+	}
+
+	public LocalDateTime getStart() {
+		return start;
+	}
+
+	public void setStart(LocalDateTime start) {
+		this.start = start;
+	}
+
+	public LocalDateTime getEnd() {
+		return end;
+	}
+
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+
+	public boolean isAdditionalEntries() {
+		return additionalEntries;
+	}
+
+	public void setAdditionalEntries(boolean additionalEntries) {
+		this.additionalEntries = additionalEntries;
+	}
+
+	public boolean isExtendedTimePeriod() {
+		return extendedTimePeriod;
+	}
+
+	public void setExtendedTimePeriod(boolean extendedTimePeriod) {
+		this.extendedTimePeriod = extendedTimePeriod;
+	}
 }
